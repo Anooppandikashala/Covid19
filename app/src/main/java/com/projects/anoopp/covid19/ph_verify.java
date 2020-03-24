3ad7e449176dde522510fcae9e19b6c03053f888
Aswathy-77 <aswathy0498@gmail.com>
2020-03-24 21:58:36 +0530

    xml only

---
+++ app/src/main/java/com/projects/anoopp/covid19/ph_verify.java
@@ -0,0 +1,14 @@
+package com.projects.anoopp.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ph_verify extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ph_verify);
+    }
+}
